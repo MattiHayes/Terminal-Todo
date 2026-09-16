b588b18f937f0ce4d144b92b22fa263e5c83c6be
MattiHayes <mattihayes145@gmail.com>
2026-09-16 22:38:49 +0100

    move app code in to main.py

--- main.py
+++ main.py
@@ -2,40 +2,8 @@ import json
 import atexit
 import signal
 from todoList import TodoList
+from app import TodoApp
 
-OPTIONS = [
-    "Add task",
-    "Mark task complete",
-    "Mark task incomplete",
-    "Remove task",
-    "Remove all complete tasks",
-    "quit"
-]
-
-def get_instruction() -> int:
-    print("Select option:")
-    while True:
-        for i, opt in enumerate(OPTIONS):
-            print(f"{i+1}: {opt}")
-        try:
-            option = int(input("> "))
-            if 1 <= option <= len(OPTIONS):
-                return option
-            print("Please select a Valid option")    
-        except ValueError:
-            print("Please input a whole number")
-
-
-def get_task_num(num_tasks: int) -> int:
-    print("Select Task")
-    while True:
-        try:
-            task_num = int(input("> "))
-            if 0 < task_num <=  num_tasks:
-                return task_num
-            print("invalid task")
-        except ValueError:
-            print("Please input the number of the task")
 
 def exit_handler(todo: TodoList):
     if len(todo) > 0:
@@ -46,8 +14,7 @@ def ctrl_c_handler(signum, frame):
     print("\nCTRL-c was pressed. Exiting")
     exit(1)
 
-
-def main():
+if __name__ == "__main__":
     todo = TodoList()
     # load tasks
     try: 
@@ -59,39 +26,5 @@ def main():
     # register handler for CTRL-c
     signal.signal(signal.SIGINT, ctrl_c_handler)
 
-    # main loop
-    while True:
-        print("To DO:")
-        print("---------------------")
-        print(todo)
-        print("---------------------")
-        option = get_instruction()
-
-        if len(todo) == 0 and (1 < option < len(OPTIONS)):
-            print("Todo list is empty")
-            continue
-
-        match option:
-            case 1:
-                print("Input task")
-                task_name = input("> ")
-                todo.add_task(task_name)
-            case 2:
-                task_num = get_task_num(len(todo))
-                todo.mark_complete(task_num)
-            case 3:
-                task_num = get_task_num(len(todo))
-                todo.mark_incomplete(task_num)
-            case 4:
-                task_num = get_task_num(len(todo))
-                todo.remove(task_num)
-            case 5:
-                todo.remove_complete()
-            case 6:
-                print("Bye :)")
-                break
-            case _:
-                raise(ValueError("Input option out of range"))
-        
-if __name__ == "__main__":
-    main()
+    app = TodoApp(todo)
+    app.run()
